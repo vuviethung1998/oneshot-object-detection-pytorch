dfd021b20ffa51fde915a20b489e69fb7d7c396f
baoanh1310 <dataholicguy@gmail.com>
2021-08-30 18:10:38 +0700

    update

---
+++ inference_net.py
@@ -0,0 +1,295 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import _init_paths
+import os
+import sys
+import numpy as np
+import argparse
+import pprint
+import pdb
+import time
+import cv2
+import imutils
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.optim as optim
+
+import torchvision.transforms as transforms
+import torchvision.datasets as dset
+from scipy.misc import imread
+from roi_data_layer.roidb import combined_roidb
+from roi_data_layer.roibatchLoader import roibatchLoader
+from model.utils.config import cfg, cfg_from_file, cfg_from_list, get_output_dir
+from model.rpn.bbox_transform import clip_boxes
+from model.nms.nms_wrapper import nms
+from model.rpn.bbox_transform import bbox_transform_inv
+from model.utils.net_utils import save_net, load_net, vis_detections
+from model.utils.blob import im_list_to_blob
+from model.faster_rcnn.vgg16 import vgg16
+from model.faster_rcnn.resnet import resnet
+import pdb
+
+
+def parse_args():
+    """
+    Parse input arguments
+    """
+    parser = argparse.ArgumentParser(description='Train a Fast R-CNN network')
+    parser.add_argument('--dataset', dest='dataset',
+                        help='training dataset',
+                        default='pascal_voc', type=str)
+    parser.add_argument('--cfg', dest='cfg_file',
+                        help='optional config file',
+                        default='cfgs/vgg16.yml', type=str)
+    parser.add_argument('--net', dest='net',
+                        help='vgg16, res50, res101, res152',
+                        default='res101', type=str)
+    parser.add_argument('--set', dest='set_cfgs',
+                        help='set config keys', default=None,
+                        nargs=argparse.REMAINDER)
+    parser.add_argument('--load_dir', dest='load_dir',
+                        help='directory to load models',
+                        default="/srv/share/jyang375/models")
+    parser.add_argument('--image_dir', dest='image_dir',
+                        help='directory to load images for demo',
+                        default="images")
+    parser.add_argument('--cuda', dest='cuda',
+                        help='whether use CUDA',
+                        action='store_true')
+    parser.add_argument('--mGPUs', dest='mGPUs',
+                        help='whether use multiple GPUs',
+                        action='store_true')
+    parser.add_argument('--cag', dest='class_agnostic',
+                        help='whether perform class_agnostic bbox regression',
+                        action='store_true')
+    parser.add_argument('--parallel_type', dest='parallel_type',
+                        help='which part of model to parallel, 0: all, 1: model before roi pooling',
+                        default=0, type=int)
+    parser.add_argument('--checksession', dest='checksession',
+                        help='checksession to load model',
+                        default=1, type=int)
+    parser.add_argument('--checkepoch', dest='checkepoch',
+                        help='checkepoch to load network',
+                        default=1, type=int)
+    parser.add_argument('--checkpoint', dest='checkpoint',
+                        help='checkpoint to load network',
+                        default=10021, type=int)
+    parser.add_argument('--bs', dest='batch_size',
+                        help='batch_size',
+                        default=1, type=int)
+    parser.add_argument('--vis', dest='vis',
+                        help='visualization mode',
+                        action='store_true')
+    parser.add_argument('--webcam_num', dest='webcam_num',
+                        help='webcam ID number',
+                        default=-1, type=int)
+
+    args = parser.parse_args()
+    return args
+
+lr = cfg.TRAIN.LEARNING_RATE
+momentum = cfg.TRAIN.MOMENTUM
+weight_decay = cfg.TRAIN.WEIGHT_DECAY
+
+def _get_image_blob(im):
+    """
+        Converts an image into a network input.
+        Arguments:
+            im (ndarray): a color image in BGR order
+        Returns:
+            blob (ndarray): a data blob holding an image pyramid
+            im_scale_factors (list): list of image scales (relative to im) used
+            in the image pyramid
+    """
+
+    im_orig = im.astype(np.float32, copy=True)
+    im_orig -= cfg.PIXEL_MEANS
+
+    im_shape = im_orig.shape
+    im_size_min = np.min(im_shape[0:2])
+    im_size_max = np.max(im_shape[0:2])
+
+    processed_ims = []
+    im_scale_factors = []
+
+    for target_size in cfg.TEST.SCALES:
+        im_scale = float(target_size) / float(im_size_min)
+        # Prevent the biggest axis from being more than MAX_SIZE
+        if np.round(im_scale * im_size_max) > cfg.TEST.MAX_SIZE:
+            im_scale = float(cfg.TEST.MAX_SIZE) / float(im_size_max)
+        im = cv2.resize(im_orig, None, None, fx=im_scale, fy=im_scale,
+                interpolation=cv2.INTER_LINEAR)
+        im_scale_factors.append(im_scale)
+        processed_ims.append(im)
+
+    # Create a blob to hold the input images
+    blob = im_list_to_blob(processed_ims)
+
+    return blob, np.array(im_scale_factors)
+
+def get_query_img():
+    pass
+
+def get_input_img():
+    pass
+
+if __name__=="__main__":
+    args = parse_args()
+
+    if args.cfg_file is not None:
+        cfg_from_file(args.cfg_file)
+    if args.set_cfgs is not None:
+        cfg_from_list(args.set_cfgs)
+
+    cfg.USE_GPU_NMS = args.cuda
+
+    print('Using config:')
+    pprint.pprint(cfg)
+    np.random.seed(cfg.RNG_SEED)
+
+    input_dir = args.load_dir + "/" + args.net + "/" + args.dataset
+    if not os.path.exists(input_dir):
+        raise Exception('There is no input directory for loading network from ' + input_dir)
+    load_name = os.path.join(input_dir,
+        'faster_rcnn_{}_{}_{}.pth'.format(args.checksession, args.checkepoch, args.checkpoint))
+
+    drug_classes = np.asarray([i for  i in range(10)])
+
+      # initilize the network here.
+    if args.net == 'vgg16':
+        fasterRCNN = vgg16(drug_classes, pretrained=False, class_agnostic=args.class_agnostic)
+    elif args.net == 'res101':
+        fasterRCNN = resnet(drug_classes, 101, pretrained=False, class_agnostic=args.class_agnostic)
+    elif args.net == 'res50':
+        fasterRCNN = resnet(drug_classes, 50, pretrained=False, class_agnostic=args.class_agnostic)
+    elif args.net == 'res152':
+        fasterRCNN = resnet(drug_classes, 152, pretrained=False, class_agnostic=args.class_agnostic)
+    else:
+        print("network is not defined")
+        pdb.set_trace()
+
+    fasterRCNN.create_architecture()
+
+    print("load checkpoint %s" % (load_name))
+    if args.cuda > 0:
+        checkpoint = torch.load(load_name)
+    else:
+        checkpoint = torch.load(load_name, map_location=(lambda storage, loc: storage))
+    fasterRCNN.load_state_dict(checkpoint['model'])
+    if 'pooling_mode' in checkpoint.keys():
+        cfg.POOLING_MODE = checkpoint['pooling_mode']
+
+    print('load model successfully!')
+
+    # pdb.set_trace()
+
+    print("load checkpoint %s" % (load_name))
+
+    # initilize the tensor holder here.
+    im_data = torch.FloatTensor(1)
+    im_info = torch.FloatTensor(1)
+    query =  torch.FloatTensor(1)
+    num_boxes = torch.LongTensor(1)
+    gt_boxes = torch.FloatTensor(1)
+
+    # ship to cuda
+    if args.cuda:
+        cfg.CUDA = True
+        fasterRCNN.cuda()
+        im_data = im_data.cuda()
+        query = query.cuda()
+        im_info = im_info.cuda()
+        num_boxes = num_boxes.cuda()
+        gt_boxes = gt_boxes.cuda()
+
+    # make variable
+    with torch.no_grad():
+        im_data = Variable(im_data)
+        query = Variable(query)
+        im_info = Variable(im_info)
+        num_boxes = Variable(num_boxes)
+        gt_boxes = Variable(gt_boxes)
+
+    if args.cuda > 0:
+        cfg.CUDA = True
+
+    if args.cuda > 0:
+        fasterRCNN.cuda()
+
+
+    start = time.time()
+    # visualization
+    vis = args.vis
+    if vis:
+        thresh = 0.05
+    else:
+        thresh = 0.0
+    max_per_image = 100
+
+    fasterRCNN.eval()
+
+    query_img, query_label, num_images = get_query_img()
+    input_img = get_input_img()
+
+    while (num_images >= 0):
+        total_tic = time.time()
+        num_images -= 1
+
+        # read image 
+        input_im_file = input_img.copy() # anh dau vao 
+        query_im_file = os.path.join(args.image_dir, query_img[num_images]) # doc file tu warehouse 
+        
+        im_query = np.array(imread(query_im_file))
+        im_in = np.array(imread(input_im_file))
+
+        if len(im_query.shape) == 2:
+            im_query = im_query[:,:,np.newaxis]
+            im_query = np.concatenate((im_query,im_query,im_query), axis=2)
+        # rgb -> bgr
+        im_query = im_query[:,:,::-1]
+        
+        if len(im_in.shape) == 2:
+            im_in = im_in[:,:,np.newaxis]
+            im_in = np.concatenate((im_in,im_in,im_in), axis=2)
+        # rgb -> bgr
+        im_in = im_in[:,:,::-1]
+
+        im = im_in.copy()
+        blobs, im_scales = _get_image_blob(im)
+        assert len(im_scales) == 1, "Only single-image batch implemented"
+        im_blob = blobs
+        im_info_np = np.array([[im_blob.shape[1], im_blob.shape[2], im_scales[0]]], dtype=np.float32)
+
+        im_data_pt = torch.from_numpy(im_blob)
+        im_data_pt = im_data_pt.permute(0, 3, 1, 2)
+        im_info_pt = torch.from_numpy(im_info_np)
+
+        im_data.data.resize_(im_data_pt.size()).copy_(im_data_pt)
+        im_info.data.resize_(im_info_pt.size()).copy_(im_info_pt)
+
+        query_im = im_query.copy()
+        blobs_query, im_scales_query = _get_image_blob(query_im)
+        assert len(im_scales_query) == 1, "Only single-image batch implemented"
+        im_query_blob = blobs_query
+        im_query_info_np = np.array([[im_query_blob.shape[1], im_query_blob.shape[2], im_scales_query[0]]], dtype=np.float32)
+
+        im_query_data_pt = torch.from_numpy(im_query_blob)
+        im_query_data_pt = im_query_data_pt.permute(0, 3, 1, 2)
+        im_query_info_pt = torch.from_numpy(im_query_info_np)
+
+        query.data.resize_(im_query_data_pt.size()).copy_(im_query_data_pt)
+
+        gt_boxes.data.resize_(1, 1, 5).zero_()
+        num_boxes.data.resize_(1).zero_()
+
+        # pdb.set_trace()
+        det_tic = time.time()
+
+        rois, cls_prob, bbox_pred, \
+        rpn_loss_cls, rpn_loss_box, \
+        RCNN_loss_cls, _, RCNN_loss_bbox, \
+        rois_label, weight = fasterRCNN(im_data, query, im_info, gt_boxes)
+
